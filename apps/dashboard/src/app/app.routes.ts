b02c4e144a7c98fba9be9c4be7859bcdc1eb9687
Jatin <jatin10269@gmail.com>
2024-09-07 14:32:40 -0400

    made dynamic

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
+import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
   {
     path: 'login',
-    loadChildren: () => import('login/Routes').then((m) => m.remoteRoutes),
+    loadChildren: () =>
+    loadRemoteModule('login', './Routes').then((m) => m.remoteRoutes),
   },
   {
     path: '',
