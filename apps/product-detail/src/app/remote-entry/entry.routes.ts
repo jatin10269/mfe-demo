bdfd3e863da810c0669e80793f61af776bbb0f12
Lokesh Babu Mani <lokezhbabuofficial@gmail.com>
2024-09-17 06:08:37 -0400

    clean up the projects

--- apps/product-detail/src/app/remote-entry/entry.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-import { RemoteEntryComponent } from './entry.component';
-import { DetailComponent } from '../components/detail/detail.component';
-
-export const remoteRoutes: Route[] = [
-  { path: '', component: DetailComponent },
-];
